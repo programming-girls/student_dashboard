9e904be377fe3e517129644b26553cd9ab14bfd1
Adah Milly <oamildred@gmail.com>
2022-02-11 16:11:13 +0300

    ft home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Welcome to Eshirali School</div>;
+  return (
+    <div>
+      <div className="parent">
+        <header className="nav">
+          <p>Eshirali School</p>
+          <button>Logout</button>
+          <button>Login</button>
+        </header>
+        <div className="main-content">
+          <p>Main content</p>
+        </div>
+        <footer className="footer">
+          <p>Eshirali school</p>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
