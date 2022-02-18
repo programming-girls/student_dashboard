fd09f66ebcbc4b39bf6ec8527e9367ccb63c4c48
Adah Milly <oamildred@gmail.com>
2022-02-18 12:56:38 +0300

    ft login route

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import LogIn from "./components/LogIn/LogIn";
 import NavBar from "./components/NavBar/NavBar";
 import SignUp from "./components/SignUp/SignUp";
 
-import { Card } from "@mui/material";
+
 
 function App() {
   return (
     <Router>
-      <Card>
-        <div className="app">
-          <NavBar />
-          <div className="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/login" element={<LogIn />} />
-            </Routes>
-          </div>
+      <div className="app">
+        <NavBar />
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+          </Routes>
         </div>
-      </Card>
+      </div>
     </Router>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,24 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { AppBar, Typography, Toolbar, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-  },
-  linkStyle: {
-    color: "#fafafa",
-    textDecoration: "none",
-  },
-});
 
 const CustomButton = styled(Button)(({ theme }) => ({
   color: "#fff",
 }));
 
 const NavBar = () => {
-  const classes = useStyles();
   const navigate = useNavigate();
 
   const handleGetStarted = () => {
@@ -30,18 +18,14 @@ const NavBar = () => {
     <>
       <AppBar className="header" position="static" color={"primary"}>
         <Toolbar>
-          <Typography variant="h5" className={classes.root}>
-            <h5 className={classes.linkStyle} to="/">
-              Eshiralli School
-            </h5>
+          <Typography variant="h5">
+            <h5 to="/">Eshiralli School</h5>
           </Typography>
           <CustomButton variant={"outlined"} onClick={() => handleGetStarted()}>
             <Link to="/signup">Get Started</Link>
           </CustomButton>
           <CustomButton variant={"outlined"}>
-            <Link className={classes.linkStyle} to="/login">
-              LogIn
-            </Link>
+            <Link to="/login">LogIn</Link>
           </CustomButton>
         </Toolbar>
       </AppBar>
