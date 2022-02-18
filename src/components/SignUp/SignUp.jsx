d13d37ea9d2a6adf6422b44e48cf365aabb55705
Adah Milly <oamildred@gmail.com>
2022-02-18 15:43:10 +0300

    ft signup form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,6 +1,37 @@
 import React from "react";
+import { TextField } from "@mui/material";
+import { Container } from "@mui/material";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "75%",
+        gap: "20px",
+      }}>
+      <h1>Sign Up</h1>
+      <TextField
+        variant="outlined"
+        label="First Name"
+        autoComplete="name"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        label="Last Name"
+        autoComplete="name"
+        autoFocus
+      />
+      <TextField
+        variant="outlined"
+        label="Email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField variant="outlined" label="Password" autoFocus />
+    </Container>
+  );
 };
 export default SignUp;
