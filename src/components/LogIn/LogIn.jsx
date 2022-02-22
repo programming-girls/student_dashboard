9b98bf6af439f3c4b5ec7eb2e16cd8c6b9a4cfb7
Adah Milly <oamildred@gmail.com>
2022-02-22 10:32:26 +0300

    ft navbar

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { TextField } from "@mui/material";
+import { Box } from "@mui/material";
 
-const LogIn = () => {
-  return <div>LogIn</div>;
+import { Button } from "@mui/material";
+
+const SignUp = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "50%",
+        gap: "20px",
+        margin: "auto",
+      }}>
+      <h1 style={{ textAlign: "center" }}>Login</h1>
+      <TextField
+        variant="outlined"
+        label="Email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField variant="outlined" label="Password" autoFocus />
+      <Button variant="contained">Login</Button>
+    </Box>
+  );
 };
-
-export default LogIn;
+export default SignUp;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { AppBar, Typography, Toolbar, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import "./NavBar.css";
 
 const CustomButton = styled(Button)(({ theme }) => ({
   color: "#fff",
@@ -16,17 +17,25 @@ const NavBar = () => {
   };
   return (
     <>
-      <AppBar className="header" position="static" color={"primary"}>
+      <AppBar className="header" color={"primary"}>
         <Toolbar>
-          <Typography variant="h5">
-            <h5 to="/">Eshiralli School</h5>
-          </Typography>
-          <CustomButton variant={"outlined"} onClick={() => handleGetStarted()}>
-            <Link to="/signup">Get Started</Link>
-          </CustomButton>
-          <CustomButton variant={"outlined"}>
-            <Link to="/login">LogIn</Link>
-          </CustomButton>
+          <div className="parent">
+            <div className="l-side">
+              <Typography variant="h5">
+                <h5 to="/">Eshiralli School</h5>
+              </Typography>
+            </div>
+            <div className="r-side">
+              <CustomButton
+                variant={"outlined"}
+                onClick={() => handleGetStarted()}>
+                <Link to="/signup">Get Started</Link>
+              </CustomButton>
+              <CustomButton variant={"outlined"}>
+                <Link to="/login">LogIn</Link>
+              </CustomButton>
+            </div>
+          </div>
         </Toolbar>
       </AppBar>
     </>
